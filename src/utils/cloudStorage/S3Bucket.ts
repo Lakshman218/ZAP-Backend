5da7b57b815991dd8df468fa419dc6a5fcc108cb
LAKSHMAN S <lakshmans218@gmail.com>
2024-07-03 12:57:08 +0530

    issue found

--- src/utils/cloudStorage/S3Bucket.ts
+++ src/utils/cloudStorage/S3Bucket.ts
@@ -1,14 +1,10 @@
+require('dotenv').config();
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
-// const bucketName = process.env.BUCKET_NAME as string;
-// const region = process.env.BUCKET_REGION as string;
-// const accessKeyId = process.env.ACCESS_KEY as string;
-// const secretAccessKey = process.env.SECRET_KEY as string;
-
-const bucketName = 'zap-users-chat-files'
-const region = 'ap-south-1'
-const accessKeyId = 'AKIA4CBA6FAE6YYPXS4D'  
-const secretAccessKey = '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'
+const bucketName = process.env.BUCKET_NAME as string;
+const region = process.env.BUCKET_REGION as string;
+const accessKeyId = process.env.ACCESS_KEY as string;
+const secretAccessKey = process.env.SECRET_KEY as string;
 
 console.log(bucketName, region, accessKeyId, secretAccessKey);
 
@@ -22,15 +18,18 @@ const s3Client = new S3Client({
 
 export const s3Upload = async (file: any) => {
   const params:any = {
-      Bucket: bucketName,
-      Key: file.originalname,
-      Body: file.buffer,
-      ContentType: file.mimetype,
+    Bucket: bucketName,
+    Key: file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype,
   };
 
-  await s3Client.send(new PutObjectCommand(params));
-
-  const url = `https://${bucketName}.s3.${region}.amazonaws.com/${file.originalname}`;
-
-  return url;
+  try {
+    await s3Client.send(new PutObjectCommand(params));
+    const url = `https://${bucketName}.s3.${region}.amazonaws.com/${file.originalname}`;
+    return url;
+  } catch (error) {
+    console.error("Error uploading file to S3:", error);
+    throw error;
+  }
 };
